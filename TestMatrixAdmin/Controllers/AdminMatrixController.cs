358b5f8de92fec5d3b5e49630fb5553b050cae5a
ERICK SEBASTIAN CARLOSAMA TABANGO <95940228+Sebas21EC@users.noreply.github.com>
2024-01-24 14:59:55 -0500

    Se agrega el proeycto con adapacion del home y la navbar

---
+++ TestMatrixAdmin/Controllers/AdminMatrixController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestMatrixAdmin.Controllers
+{
+    public class AdminMatrixController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult FormWizard()
+        {
+            //la vista se llema formsView
+            return View("MatrixAdmin/FormWizard/FormsView");
+        }
+
+    }
+}
