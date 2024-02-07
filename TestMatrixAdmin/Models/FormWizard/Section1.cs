6cff7a5af9efccff632d2786217b9a418a1e07cb
ERICK SEBASTIAN CARLOSAMA TABANGO <95940228+Sebas21EC@users.noreply.github.com>
2024-02-07 16:50:38 -0500

    Secction1 para nuevo usuario para nuevos usuariso validando en la  parte del Backend, Listo

--- TestMatrixAdmin/Models/FormWizard/Section1.cs
+++ TestMatrixAdmin/Models/FormWizard/Section1.cs
@@ -1,28 +1,60 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
 
 namespace TestMatrixAdmin.Models
 {
-    public class Section1
+    public class Section1 : IValidatableObject
     {
-        [Required(ErrorMessage = "Please enter your ID number")]
+        [Required(ErrorMessage = "Please enter your ID number!")]
         [RegularExpression(@"^\d{10}$", ErrorMessage = "Please enter a valid ID number")]
         public string? IdNumber { get; set; }
 
         [Required(ErrorMessage = "Please enter select one option")]
-        public bool? ContactOption { get; set; }
+        public string? ContactOption { get; set; }
 
-        [Required(ErrorMessage = "Please enter your figer print code")]
+        [Required(ErrorMessage = "Please enter your figer print code!")]
+        [RegularExpression(@"^[a-zA-Z0-9]{10}$", ErrorMessage = "Please enter a valid figer print code")]
         public string? FingerprintCode { get; set; }
 
-        [Required(ErrorMessage = "Please enter your phone number")]
+        //[Required(ErrorMessage = "Please enter your phone number")
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Please enter a valid phone number")]
         public string? PhoneNumber { get; set; }
 
-        [Required(ErrorMessage = "Please enter your email address")]
+        //[Required(ErrorMessage = "Please enter your email address")]
         [EmailAddress(ErrorMessage = "Please enter a valid email address")]
         public string? Email { get; set; }
 
-        [Required(ErrorMessage = "Please confirm your email address")]
+        //[Required(ErrorMessage = "Please confirm your email address")]
         [EmailAddress(ErrorMessage = "Please enter a valid email address")]
         public string? EmailConfirm { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (ContactOption == "phone")
+            {
+                if (string.IsNullOrEmpty(PhoneNumber))
+                {
+                    yield return new ValidationResult("Please enter your phone number.", new[] { nameof(PhoneNumber) });
+                }
+            }
+            else if (ContactOption == "email")
+            {
+                if (string.IsNullOrEmpty(Email))
+                {
+                    yield return new ValidationResult("Please enter your email address.", new[] { nameof(Email) });
+                }
+                if (Email != EmailConfirm)
+                {
+                    yield return new ValidationResult("The email and confirmation email do not match.", new[] { nameof(EmailConfirm) });
+                }
+            }
+            else
+            {
+                // Opción para cuando no se selecciona ninguna opción de contacto
+                yield return new ValidationResult("Please select a contact option.", new[] { nameof(ContactOption) });
+            }
+        }
+
     }
 }
--- TestMatrixAdmin/obj/Debug/net7.0/TestMatrixAdmin.AssemblyInfo.cs
+++ TestMatrixAdmin/obj/Debug/net7.0/TestMatrixAdmin.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestMatrixAdmin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+358b5f8de92fec5d3b5e49630fb5553b050cae5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fb6d949cf346380fa62fcf9e98c4a0c666d5b54")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestMatrixAdmin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestMatrixAdmin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
